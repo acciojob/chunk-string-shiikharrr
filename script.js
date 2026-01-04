28ecaf877d57d41d6e3156bfa7bccc71bd471ab4
shiikharrr <69209995+shiikharrr@users.noreply.github.com>
2026-01-04 19:29:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function stringChop(str, size) {
-  // your code here
+  if (str == null)
+	  return [];
+	size = Number(size);
+	if (!Number.isInteger(size) || size <= 0)
+		return[];
+
+	const out = [];
+	for (let i  = 0; i < str.length; i += size){
+		out.push(str.slice(i, i + size));
+	}
+	return out;
 }
 
 // Do not change the code below
